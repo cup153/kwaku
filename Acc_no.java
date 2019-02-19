3ac33916de4d15209896501d5eeb667c987854f4
cup153 <kwaku.nketiah@st.vvu.edu.gh>
2019-02-19 19:14:22 +0000

    Add files via upload

---
+++ Acc_no.java
@@ -0,0 +1,54 @@
+class Acc_no{
+
+
+ int deposit(){
+      String get_accinfo(String name, double acc_bal );
+
+
+     int deposit();{
+      set acc_info();
+   
+
+   int withdraw();{
+      public  String get_accinfo(String name, double acc_bal);
+
+     int withdraw();{
+        set acc_info();
+   
+
+  int transfer();{
+     public  String get_accinfo(String name, double acc_bal);
+
+      int transfer();{
+     	set acc_info();
+
+
+   public void String getacc_info(name, init_bal, deposit, withdraw, transfer, acc_bal);
+
+   }
+    }
+      }
+        }
+          }
+            }
+              }
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BankAccounts.java
@@ -0,0 +1,73 @@
+class BankAccounts{
+
+ String name = Kwaku;
+ double init_bal = 45000;
+ int deposit = 100000;
+ int withdraw = 50000;
+ int transfer = 20000;
+ int Get_accinfo = name, init_bal, acc_no, deposit, withdraw, transfer, acc_bal;
+ double acc_bal = 40000;
+
+ int deposit();
+ int withdraw();
+ int transfer();
+ int Get_accinfo();
+ double acc_bal();
+ String name();
+ int init_bal();
+ 
+
+
+
+ int Get_accinfo();{
+   System.out.println("Store account information");
+
+
+
+ String name();{
+    System.out.println("What is your name");
+
+ int init_bal();{
+	System.out.println("What is your initial balance");
+
+ int acc_no();{
+	System.out.println("What is your account info");
+
+ int deposit();{
+    System.out.println("How much do you want to deposit");
+
+
+
+ int withdraw();{
+    System.out.println("How much do you want to withdraw");
+
+  int transfer();{
+    System.out.println("How much do you want to transfer");
+
+
+    public String Getacc_info(name, init_bal, deposit, withdraw, transfer, acc_bal);
+
+
+
+ 
+}
+
+   }
+
+      }
+
+          }
+              }
+
+                  }
+                     
+                     }
+
+                       }
+
+                          
+     
+
+
+   
+
